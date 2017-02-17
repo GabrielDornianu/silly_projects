2f6ac20d8dfa106b683a4e3b4a666358730c1dfc
gabrieldornianu <gabrieldornianu@yahoo.com>
2017-02-17 12:03:15 +0200

    You can now play with the computer

--- tic_tac_toe/javascript.js
+++ tic_tac_toe/javascript.js
@@ -10,35 +10,41 @@ var canvas = document.getElementById("canvas"),
 	your_square = document.getElementById("your_square"),
 	squares = [],
 	actual_square_data,
-	player_starts = true,
 	this_move = "player",
 	actual_symbol = "X",
-	difficulty = "easy";
+	difficulty = "easy",
+	frees_quares = [],
+	game_difficulty = 1;
 
-function scan_table(symbol){
+function set_difficulty(difficulty){
+	game_difficulty = difficulty;
+}
+
+function scan_table(){
 	var winner;
-	if(symbol != "X"){
-		winner = "player1";
-	}else if(symbol != "0"){
-		winner = "player2";
+	if(actual_symbol == "X"){
+		winner = "player";
+	}else if(actual_symbol == "0"){
+		winner = "computer";
 	}
-	if(squares[0].value == symbol && squares[1].value == symbol && squares[2].value == symbol){
+	if(squares[0].value == actual_symbol && squares[1].value == actual_symbol && squares[2].value == actual_symbol){
 		alert("The " + winner + " has won!");
-	}else if(squares[3].value == symbol && squares[4].value == symbol && squares[5].value == symbol){
+	}else if(squares[3].value == actual_symbol && squares[4].value == actual_symbol && squares[5].value == actual_symbol){
 		alert("The " + winner + " has won!");
-	}else if(squares[6].value == symbol && squares[7].value == symbol && squares[8].value == symbol){
+	}else if(squares[6].value == actual_symbol && squares[7].value == actual_symbol && squares[8].value == actual_symbol){
 		alert("The " + winner + " has won!");
-	}else if(squares[0].value == symbol && squares[3].value == symbol && squares[6].value == symbol){
+	}else if(squares[0].value == actual_symbol && squares[3].value == actual_symbol && squares[6].value == actual_symbol){
 		alert("The " + winner + " has won!");
-	}else if(squares[1].value == symbol && squares[4].value == symbol && squares[7].value == symbol){
+	}else if(squares[1].value == actual_symbol && squares[4].value == actual_symbol && squares[7].value == actual_symbol){
 		alert("The " + winner + " has won!");
-	}else if(squares[2].value == symbol && squares[5].value == symbol && squares[8].value == symbol){
+	}else if(squares[2].value == actual_symbol && squares[5].value == actual_symbol && squares[8].value == actual_symbol){
 		alert("The " + winner + " has won!");
-	}else if(squares[0].value == symbol && squares[4].value == symbol && squares[8].value == symbol){
+	}else if(squares[0].value == actual_symbol && squares[4].value == actual_symbol && squares[8].value == actual_symbol){
 		alert("The " + winner + " has won!");
-	}else if(squares[2].value == symbol && squares[4].value == symbol && squares[6].value == symbol){
+	}else if(squares[2].value == actual_symbol && squares[4].value == actual_symbol && squares[6].value == actual_symbol){
 		alert("The " + winner + " has won!");
 	}
+
 }
 
 function draw_X(obj, color){
@@ -76,19 +82,18 @@ function draw_symbol(square, symbol){
 //Each time the player or the computer makes a move , this function will get used
 function select_square(){
 	if(actual_square_data.value == null){
-		console.log("square number " + actual_square_data.square_number + " selected");
-		actual_square_data.value = actual_symbol;
-		draw_symbol(actual_square_data, actual_symbol);
-		console.log(squares[actual_square_data.square_number-1].value);
 		if(actual_symbol == "X"){
+			draw_symbol(actual_square_data, actual_symbol);
+			actual_square_data.value = actual_symbol;
 			squares[actual_square_data.square_number-1].value = "0";
 			actual_symbol = "0";
-		}else{
-			squares[actual_square_data.square_number-1].value = "X";
-			actual_symbol = "X";
+			//scan_table();
+			if(game_difficulty == 1){
+				easy_AI_move();
+			}
+			console.log(actual_square_data.value);
 		}
 	}
-	scan_table(actual_symbol);
 }
 
 
@@ -130,8 +135,6 @@ for(let i=1; i<10; i++){
 	}
 }
 
-//console.log(squares);
-
 })();
 
 //This function will draw the lines of the game
@@ -165,7 +168,6 @@ function highlight_actual_square(){
 	for(let square in squares){
 		if((currentY >= squares[square].lower_square_coordY && currentY <= squares[square].square_coordY) && (currentX >= squares[square].lower_square_coordX && currentX <= squares[square].square_coordX)){
 			your_square.innerHTML = squares[square].square_number;
-			//console.log(squares[square].lower_square_coordX + " " + squares[square].lower_square_coordY);
 			actual_square_data = squares[square];
 		}
 	}
@@ -175,6 +177,23 @@ highlight_actual_square();
 
 }
 
-function AI_move(){
+function easy_AI_move(){
+	frees_quares = squares.filter(function(square){
+		if(square.value==null){
+			return true;
+		}
+	});
+	let random_number = Math.floor(Math.random()*frees_quares.length);
+	draw_symbol(frees_quares[random_number], "0");
+	squares[frees_quares[random_number].square_number-1].value = "0";
+	actual_symbol = "X";
+	scan_table();
+}
+
+function medium_AI_move(){
+
+}
+
+function hard_AI_move(){
 
 }
